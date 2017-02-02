1bc2a9636ba7e08f133f099957ef2b1ca7228399
Radostan Riedel <raybuntu@googlemail.com>
2017-02-02 11:31:31 +0100

    tvheadend42: update rev to 109.2

--- packages/addons/service/tvheadend42/package.mk
+++ packages/addons/service/tvheadend42/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="tvheadend42"
 PKG_VERSION="e5f5a42"
 PKG_VERSION_NUMBER="4.1.2415"
-PKG_REV="109.1"
+PKG_REV="109.2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
