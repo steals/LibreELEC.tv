574425b207a81f521ccccfedf42101193dbd725f
Jamie Coldhill <wrxtasy@amnet.net.au>
2017-06-23 12:10:40 +0800

    media-build: amlvideo and meson-ir support

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -31,6 +31,12 @@ PKG_LONGDESC="DVB drivers that replace the version shipped with the kernel"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+if [ "$TARGET_KERNEL_ARCH" = "arm64" -a "$TARGET_ARCH" = "arm" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gcc-linaro-aarch64-linux-gnu:host"
+  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-linux-gnu/bin/:$PATH
+  TARGET_PREFIX=aarch64-linux-gnu-
+fi
+
 pre_make_target() {
   export KERNEL_VER=$(get_module_dir)
   export LDFLAGS=""
@@ -44,13 +50,31 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  else
+  elif [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
     if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
     fi
   fi
 
   # add menuconfig to edit .config
+
+  # Amlogic AMLVIDEO driver
+  if [ -e "$(kernel_path)/drivers/amlogic/video_dev" ]; then
+
+    # Copy, patch and enable amlvideodri module
+    cp -a "$(kernel_path)/drivers/amlogic/video_dev" "linux/drivers/media/"
+    sed -i 's,common/,,g; s,"trace/,",g' $(find linux/drivers/media/video_dev/ -type f)
+    sed -i 's,\$(CONFIG_V4L_AMLOGIC_VIDEO),m,g' "linux/drivers/media/video_dev/Makefile"
+    echo "obj-y += video_dev/" >> "linux/drivers/media/Makefile"
+    echo "source drivers/media/video_dev/Kconfig " >> "linux/drivers/media/Kconfig"
+
+    # Copy and enable videobuf-res module
+    cp -a "$(kernel_path)/drivers/media/v4l2-core/videobuf-res.c" "linux/drivers/media/v4l2-core/"
+    cp -a "$(kernel_path)/include/media/videobuf-res.h" "linux/include/media/"
+    echo "obj-m += videobuf-res.o" >> "linux/drivers/media/v4l2-core/Makefile"
+
+  fi
+
   make VER=$KERNEL_VER SRCDIR=$(kernel_path)
 }
 
