tomsenior9999/LibreElec-S905

libhybris: new package
This package along with android-headers is needed to run
Kodi on top of Android 6.0 OpenGL library

packages/devel/libhybris/package.mk
@@ -1,0 +1,39 @@
+################################################################################
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
+#      Copyright (C) 2016 kszaq
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libhybris"
+PKG_VERSION="240ae56"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libhybris/libhybris/"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*/hybris"
+PKG_DEPENDS_TARGET="toolchain android-headers"
+PKG_SECTION="devel"
+PKG_SHORTDESC="libhybris: Allows to run bionic-based HW adaptations in glibc systems - libs"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-arch=$TARGET_ARCH \
+                           --with-default-egl-platform=fbdev \
+                           --with-android-headers=$ROOT/$BUILD/android-headers-23/23 \
+                           --with-default-hybris-ld-library-path=/system/lib \
+                           --enable-mali-quirks \
+                           --enable-experimental"

steals/LibreELEC.tv

packages/devel/libhybris/package.mk
@@ -1,0 +1,39 @@
+################################################################################
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
+#      Copyright (C) 2016 kszaq
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libhybris"
+PKG_VERSION="a2e5d92"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/krnlyng/libhybris/"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*/hybris"
+PKG_DEPENDS_TARGET="toolchain android-headers"
+PKG_SECTION="devel"
+PKG_SHORTDESC="libhybris: Allows to run bionic-based HW adaptations in glibc systems - libs"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-arch=$TARGET_ARCH \
+                           --with-default-egl-platform=fbdev \
+                           --with-android-headers=$BUILD/android-headers-23/23 \
+                           --with-default-hybris-ld-library-path=/system/lib \
+                           --enable-mali-quirks \
+                           --enable-experimental"